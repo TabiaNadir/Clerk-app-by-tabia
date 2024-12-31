aefc55a569a181967c971441b104a0269ed17949
Tabia Nadir <nadirumer786001@gmail.com>
2025-01-01 00:21:47 +0500

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,11 @@
+import SignInPage from "@/app/sign-in/[[...slug]]/page";
+import SignUpPage from "@/app/sign-up/[[...sign-up]]/page";
+
 const Dashboard = () => {
     return (
       <>
+      <SignInPage />
+      <SignUpPage />
         <h1 className='text-2xl font-bold mb-5'>Dashboard</h1>
         <p className='mb-5'>Welcome to the dashboard!</p>
       </>
